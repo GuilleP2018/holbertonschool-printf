c14d9b6d35b96bc2b46a666e82a6c5aff87946e8
figue0122 <miguel.figueroa0122@gmail.com>
2023-07-27 14:11:24 +0000

    changed header file

--- main.h
+++ main.h
@@ -1,18 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int _strlen(char *s);
-int _putchar(char c);
+/**
+ * struct inface - interface for print
+ * @id: the character, which identifies the type of the arg
+ * @fn: function to run if id matches 
+ */
+
+typedef struct intface
+{
+	char id;
+	int (*fn)(va_list);
+} interface;
+
 int _printf(const char *format, ...);
-int p_char(char c);
-int p_str(char *s);
-int p_int(int num);
-int p_default(char c);
-int print_char(va_list types);
-int print_string(va_list types);
-int print_percent(va_list types);
-int print_int(va_list types);
-int print_binary(va_list types);
+int _print_char(va_list ls);
+int _print_mod(va_list ls);
+int _print_string(va_list ls);
+int _primnt_int(va_list ls);
 #endif
